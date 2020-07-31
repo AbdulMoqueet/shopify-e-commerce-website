a644901643bd5c786efa07e83b5b2f1a4de1eb94
Abdul_Moqueet <moqueetabdul@gmail.com>
2020-07-31 13:18:55 +0530

    navbar styling done

--- src/components/header/NavBar.js
+++ src/components/header/NavBar.js
@@ -5,7 +5,10 @@ const NavBar = () => {
         <nav>
             <div className="row">
                 <ul>
-                    <li><a href="#All"><ion-icon className="menu-burger-icon" name="menu-outline"></ion-icon> All Category <ion-icon name="chevron-down-outline"></ion-icon></a></li>
+                    <li><a href="#All"><ion-icon style={{
+                        fontSize: "30px",
+                        marginRight: "10px"
+                        }} name="menu-outline"></ion-icon> All Category <ion-icon name="chevron-down-outline" style={{marginLeft:"20px"}}></ion-icon></a></li>
                     <li><a href="#home" class="nav-active scroll-easing">Home <ion-icon name="chevron-down-outline"></ion-icon></a></li>
                     <li><a href="#Pages" class="scroll-easing">Pages <ion-icon name="chevron-down-outline"></ion-icon></a></li>
                     <li><a href="#Products" class="scroll-easing">Products <ion-icon name="chevron-down-outline"></ion-icon></a></li>
